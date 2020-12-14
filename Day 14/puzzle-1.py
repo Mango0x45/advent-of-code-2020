35b1a09341469f796bd03dec1a41c59754d3af1c
Mango0x45 <thomasvoss@live.com>
2020-12-14 16:02:26 +0100

    Add Day 14 solutions

---
+++ Day 14/puzzle-1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+def bitmask(mask: str, num: int) -> int:
+    binary = bin(num)[2:]
+    binary = "0" * (36 - len(binary)) + binary
+    res = ""
+
+    for i in range(len(binary)):
+        if binary[i] == "1" and mask[i] == "0":
+            res += "0"
+        elif binary[i] == "0" and mask[i] == "1":
+            res += "1"
+        else:
+            res += binary[i]
+
+    return int(res, 2)
+
+
+with open("input", "r") as f:
+    lines = f.read().splitlines()
+
+mem = {}
+
+for line in lines:
+    line = line.split(" ")
+    if line[0] == "mask":
+        mask = line[2]
+    else:
+        mem[line[0][4:-1]] = bitmask(mask, int(line[2]))
+
+acc = 0
+for val in mem:
+    acc += mem[val]
+
+print(acc)
---
+++ Day 14/puzzle-2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+def bitmask(mask: str, num: int) -> tuple[int, ...]:
+    binary = bin(num)[2:]
+    binary = binary.zfill(36)
+    res = ""
+
+    for i in range(len(binary)):
+        if mask[i] == "0":
+            res += binary[i]
+        else:
+            res += mask[i]
+
+    n = res.count("X")
+    combos = tuple(bin(i)[2:].zfill(n) for i in range(2 << n - 1))
+
+    acc = []
+    for combo in combos:
+        temp = res
+        for i in range(n):
+            temp = temp.replace("X", combo[i], 1)
+        acc.append(temp)
+
+    return tuple(int(x, 2) for x in acc)
+
+
+with open("input", "r") as f:
+    lines = f.read().splitlines()
+
+mem = {}
+
+for line in lines:
+    line = line.split(" ")
+    if line[0] == "mask":
+        mask = line[2]
+    else:
+        address = line[0][4:-1]
+        addresses = bitmask(mask, int(address))
+        for a in addresses:
+            mem[a] = int(line[2])
+
+acc = 0
+for val in mem:
+    acc += mem[val]
+
+print(acc)
