21f7e0358535629070e161432fd924d55ee5c0c4
Mango0x45 <thomasvoss@live.com>
2020-12-15 13:38:19 +0100

    Add Day 15 solutions

---
+++ Day 15/puzzle-2.c
@@ -0,0 +1,35 @@
+/**
+ * The exact same as puzzle-1.c, but a different buffer size
+ */
+#include <stdio.h>
+
+#ifdef BUFSIZ
+#   undef BUFSIZ
+#endif
+#define BUFSIZ 30000000
+
+int main(void)
+{
+    /* Given starting numbers */
+    static unsigned int nums[BUFSIZ] = {0};
+    nums[6] = 1;
+    nums[19] = 2;
+    nums[0] = 3;
+    nums[5] = 4;
+    nums[7] = 5;
+    nums[13] = 6;
+
+    unsigned int temp, lnum = 1;
+    for (int i = 8; i <= BUFSIZ; i++) {
+        temp = lnum;
+        if (nums[lnum] == 0)
+            lnum = 0;
+        else
+            lnum = i - 1 - nums[lnum];
+
+        nums[temp] = i - 1;
+    }
+
+    printf("%u\n", lnum);
+    return 0;
+}
